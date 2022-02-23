35adbfe5d2b1e1a8e6f9e0088038ffbc6354b783
colinbruna <brunacbaldin@gmail.com>
2022-02-22 21:27:54 -0300

    aprendendo clone e push

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("Bom dia!");
+		System.out.println("Testando git clone");
+		System.out.println("Testanto git push");
 
 	}
-
 }
